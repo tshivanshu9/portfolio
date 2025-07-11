dbbcf7858b0158ac98a734d5e3621a36d2655a94
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-11 23:45:42 +0530

    Prettier installed

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,17 +1,16 @@
-import { RiReactjsLine } from "react-icons/ri";
-
+import { RiReactjsLine } from 'react-icons/ri';
 
 const Technologies = () => {
   return (
-    <div className='border-b border-neutral-900 pb-24'>
-      <h2 className='my-20 text-center text-4xl'>Technologies</h2>
+    <div className="border-b border-neutral-900 pb-24">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <RiReactjsLine className="text-7xl text-cyan-400" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies
+export default Technologies;
