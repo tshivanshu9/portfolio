05e886de696a0690bc819ac777b570b44f1e87c8
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-12 02:24:38 +0530

    Constants updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: '767 Fifth Avenue, New York, NY 10153 ',
-  phoneNo: '+12 4555 666 00 ',
-  email: 'me@example.com',
+  address: 'Kanpur, Uttar Pradesh, India',
+  phoneNo: '+91 9149918535',
+  email: 'tshivanshu9@gmail.com',
 };
