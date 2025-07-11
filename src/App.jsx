d0ed9ed63363c11c8c2b26fd1bd224514970b6cd
tshivanshu9 <tshivanshu9@gmail.com>
2025-07-12 01:17:48 +0530

    Added animation in Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
